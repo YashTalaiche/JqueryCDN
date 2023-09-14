bac983f0d6011366f3338494c6cd28883f2a43af
YashTalaiche <yash.talaiche@zehntech.com>
2023-09-14 17:08:19 +0530

    add minus function

--- JSCDN.js
+++ JSCDN.js
@@ -11,4 +11,10 @@ $(document).ready(function() {
         var result = parseInt(bla) * parseInt(blb);
         alert("The multiply is: " + result);
     });
+    $('#minus').click(function() {
+        var bla = $('#txt_name1').val();
+        var blb = $('#txt_name2').val();
+        var result = parseInt(bla) - parseInt(blb);
+        alert("The minus is: " + result);
+    });
 });
