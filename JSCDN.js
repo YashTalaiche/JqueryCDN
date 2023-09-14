308b411b5984a52d928497ccca6616123f1f9c54
YashTalaiche <yash.talaiche@zehntech.com>
2023-09-14 17:27:01 +0530

    egsdc

--- JSCDN.js
+++ JSCDN.js
@@ -17,4 +17,16 @@ $(document).ready(function() {
         var result = parseInt(bla) - parseInt(blb);
         alert("The minus is: " + result);
     });
+    $('#into').click(function() {
+        var bla = $('#txt_name1').val();
+        var blb = $('#txt_name2').val();
+        var result = parseInt(bla) * parseInt(blb);
+        alert("The multiply is: " + result);
+    });
+    $('#minus').click(function() {
+        var bla = $('#txt_name1').val();
+        var blb = $('#txt_name2').val();
+        var result = parseInt(bla) - parseInt(blb);
+        alert("The minus is: " + result);
+    });
 });
