f52f497592da6f1cf840b6744100d30bcc10d7fd
YashTalaiche <yash.talaiche@zehntech.com>
2023-09-14 16:50:02 +0530

    add multiply function

--- JSCDN.js
+++ JSCDN.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
         var result = parseInt(bla) + parseInt(blb);
         alert("The sum is: " + result);
     });
+    $('#into').click(function() {
+        var bla = $('#txt_name1').val();
+        var blb = $('#txt_name2').val();
+        var result = parseInt(bla) * parseInt(blb);
+        alert("The multiply is: " + result);
+    });
 });
